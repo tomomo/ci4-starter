d0dab57a66e9297a3f2fb84b611a11a7b7657dc0
tomomo <eclairpark@gmail.com>
2020-12-24 18:20:24 +0900

    informationコントローラーを作成

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,6 +32,8 @@ $routes->setAutoRoute(true);
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 
+$routes->presenter('informations', ['controller' => 'Informations']);
+
 /**
  * --------------------------------------------------------------------
  * Additional Routing
---
+++ app/Controllers/Informations.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Controllers
+ *
+ * @package CodeIgniter
+ * @author  tomomo <eclairpark@gmail.com>
+ * @license http://www.opensource.org/licenses/mit-license.html  MIT License
+ */
+
+namespace App\Controllers;
+
+/**
+ * Class Informations
+ *
+ * @package CodeIgniter
+ */
+class Informations extends BaseController
+{
+	/**
+	 * 一覧表示
+	 *
+	 * @return object View
+	 */
+	public function index()
+	{
+		echo __METHOD__;
+		return view('example.html');
+	}
+
+	/**
+	 * 詳細表示
+	 *
+	 * @param string $id ID
+	 *
+	 * @return object View
+	 */
+	public function show(string $id = null)
+	{
+		echo __METHOD__;
+		return view('example.html');
+	}
+
+	/**
+	 * 作成画面表示
+	 *
+	 * @return object View
+	 */
+	public function new()
+	{
+		echo __METHOD__;
+		return view('example.html');
+	}
+
+	/**
+	 * 編集画面表示
+	 *
+	 * @param string $id ID
+	 *
+	 * @return object View
+	 */
+	public function edit(string $id = null)
+	{
+		echo __METHOD__;
+		return view('example.html');
+	}
+
+	/**
+	 * 削除確認画面表示
+	 *
+	 * @param string $id ID
+	 *
+	 * @return object View
+	 */
+	public function remove(string $id = null)
+	{
+		echo __METHOD__;
+		return view('example.html');
+	}
+
+	/**
+	 * 作成処理
+	 *
+	 * @return void
+	 */
+	public function create()
+	{
+		echo __METHOD__;
+	}
+
+	/**
+	 * 更新処理
+	 *
+	 * @param string $id ID
+	 *
+	 * @return void
+	 */
+	public function update(string $id = null)
+	{
+		echo __METHOD__;
+	}
+
+	/**
+	 * 更新処理
+	 *
+	 * @param string $id ID
+	 *
+	 * @return void
+	 */
+	public function delete(string $id = null)
+	{
+		echo __METHOD__;
+	}
+}
